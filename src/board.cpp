110fe3832b185a649e2276a88e02f8abc06200f6
MinusKelvin <mark.carlson@minuskelvin.net>
2022-11-22 10:49:14 +1100

    reduce impact of random eval component
    size: 2253
    bench: 23735991

--- src/board.cpp
+++ src/board.cpp
@@ -207,7 +207,7 @@ struct Board {
     }
 
     int eval() {
-        int value = (zobrist & 127) - 63;
+        int value = (zobrist & 63) - 31;
         int piece_values[] = {0, 1, 3, 3, 5, 9, 0};
         for (int sq = A1; sq <= H8; sq++) {
             value += (stm & board[sq] ? 100 : -100) * piece_values[board[sq] & 7];
