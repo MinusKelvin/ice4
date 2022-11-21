e2648c5ef1adcde1fdf32ecb0656f855c5f9c59f
MinusKelvin <mark.carlson@minuskelvin.net>
2022-11-21 19:51:59 +1100

    extremely basic time management
    size: 1985
    bench: 73938059

--- src/search.cpp
+++ src/search.cpp
@@ -1,5 +1,12 @@
+double now() {
+    timespec t;
+    clock_gettime(CLOCK_MONOTONIC, &t);
+    return t.tv_sec + t.tv_nsec / 1000000000.0;
+}
+
 struct Searcher {
     uint64_t nodes;
+    double abort_time;
 
     int negamax(Board &board, Move *bestmv, int16_t alpha, int16_t beta, int16_t depth, int ply) {
         Move moves[256];
@@ -36,7 +43,9 @@ struct Searcher {
 
             Board mkmove = board;
             mkmove.make_move(moves[i]);
-            nodes++;
+            if (!(++nodes & 0xFFF) && now() > abort_time) {
+                throw 0;
+            }
             int16_t v = -negamax(mkmove, 0, -beta, -alpha, depth - 1, ply + 1);
             if (v > best) {
                 best = v;
@@ -49,15 +58,22 @@ struct Searcher {
         return best;
     }
 
-    void iterative_deepening(int max_depth=4) {
+    void iterative_deepening(double time_alotment, int max_depth=250) {
         nodes = 0;
+        abort_time = now() + time_alotment * 0.1;
+        time_alotment = now() + time_alotment * 0.02;
         Move mv;
-        for (int depth = 1; depth <= max_depth; depth++) {
-            int16_t v = negamax(ROOT, &mv, LOST, WON, depth, 0);
-            printf("info depth %d nodes %ld score cp %d pv ", depth, nodes, v);
-            mv.put();
-            putchar('\n');
-        }
+        try {
+            for (int depth = 1; depth <= max_depth; depth++) {
+                int16_t v = negamax(ROOT, &mv, LOST, WON, depth, 0);
+                printf("info depth %d nodes %ld score cp %d pv ", depth, nodes, v);
+                mv.put();
+                putchar('\n');
+                if (now() > time_alotment) {
+                    break;
+                }
+            }
+        } catch (...) {}
         printf("bestmove ");
         mv.put();
         putchar('\n');
