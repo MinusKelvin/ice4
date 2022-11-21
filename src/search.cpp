795444d8594768dd9ee122ad12fe007e3b9d9234
MinusKelvin <mark.carlson@minuskelvin.net>
2022-11-21 22:59:45 +1100

    order captures in MVV-LVA order before quiets
    size: 1793
    bench: 25975887

--- src/search.cpp
+++ src/search.cpp
@@ -5,14 +5,36 @@ struct Searcher {
         if (depth == 0) return board.eval();
 
         Move moves[256];
+        int score[256];
         int mvcount;
         if (!board.movegen(moves, mvcount)) {
             return WON;
         }
 
+        for (int i = 0; i < mvcount; i++) {
+            if (board.board[moves[i].to]) {
+                score[i] = (board.board[moves[i].to] & 7) * 8 - (board.board[moves[i].from] & 7);
+            } else {
+                score[i] = -8;
+            }
+        }
+
         int16_t best = LOST + ply;
 
         for (int i = 0; i < mvcount; i++) {
+            int best_so_far = i;
+            for (int j = i+1; j < mvcount; j++) {
+                if (score[j] > score[best_so_far]) {
+                    best_so_far = j;
+                }
+            }
+            Move tmp1 = moves[i];
+            int tmp2 = score[i];
+            moves[i] = moves[best_so_far];
+            score[i] = score[best_so_far];
+            moves[best_so_far] = tmp1;
+            score[i] = tmp2;
+
             Board mkmove = board;
             mkmove.make_move(moves[i]);
             nodes++;
