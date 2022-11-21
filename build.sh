79efe4feed4414ee8d35b620cf9a1a2db55fd825
MinusKelvin <mark.carlson@minuskelvin.net>
2022-11-22 09:00:42 +1100

    compile with -march=native flag
    size: 1987
    bench: 73938059

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ if [ -z "$1" ]; then
     chmod +x ice4
     echo Compressed size: `du -b ice4`
 else
-    g++ -DOPENBENCH -O3 src/main.cpp -o "$1"
+    g++ -DOPENBENCH -O3 -march=native src/main.cpp -o "$1"
 fi
