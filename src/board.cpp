6538a201f8edfe89af0f8b6099ab28168a27d71f
MinusKelvin <mark.carlson@minuskelvin.net>
2022-11-21 22:02:22 +1100

    fix material values being 100x too low
    size: 1693
    bench: 203498380

--- src/board.cpp
+++ src/board.cpp
@@ -203,7 +203,7 @@ struct Board {
         int value = (zobrist & 0xFF) - 127;
         int piece_values[] = {0, 1, 3, 3, 5, 9, 0};
         for (int sq = A1; sq <= H8; sq++) {
-            value += (stm & board[sq] ? 1 : -1) * piece_values[board[sq] & 7];
+            value += (stm & board[sq] ? 100 : -100) * piece_values[board[sq] & 7];
         }
         return value;
     }
