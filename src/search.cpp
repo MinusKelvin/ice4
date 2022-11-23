3aad3e4ecb28a0c2801b4ff8e43539ae1b275d1e
MinusKelvin <mark.carlson@minuskelvin.net>
2022-11-23 23:08:45 +1100

    increase lmr
    size: 3034
    bench: 749293

--- src/search.cpp
+++ src/search.cpp
@@ -101,7 +101,7 @@ struct Searcher {
             if (is_rep) {
                 v = 0;
             } else if (legals) {
-                int reduction = board.board[moves[i].to] ? 0 : legals / 12;
+                int reduction = board.board[moves[i].to] ? 0 : legals / 8;
                 v = -negamax(mkmove, scratch, -alpha-1, -alpha, depth - reduction - 1, ply + 1);
                 if (v > alpha && reduction) {
                     // reduced search failed high, re-search at full depth
