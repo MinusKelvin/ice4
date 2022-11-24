2ca0e15647cc2bccb3571424b8f77de434e5f9c1
MinusKelvin <mark.carlson@minuskelvin.net>
2022-11-24 19:35:51 +1100

    unmirrored pst
    size: 3805
    bench: 7561107

--- convert_pst.py
+++ convert_pst.py
@@ -11,7 +11,7 @@ def dump_string(piece_data, stuff):
     for value in piece_data:
         smallest = smallest if smallest < value else value
         largest = largest if largest > value else value
-        average += value / 32
+        average += value / len(piece_data)
 
     scale_factor = max((largest - smallest) / 94, 1.0)
     print(f"unpack({stuff}, \"", end="")
@@ -22,17 +22,17 @@ def dump_string(piece_data, stuff):
         print(c, end="")
     print(f"\", {scale_factor:.4}, {round(smallest)}); // average: {average:.0f}")
 
-scaled = [v[0] * 160 for v in data["pst.weight"]]
+scaled = [v[0] * -160 for v in data["pst.weight"]]
 
-dump_string(scaled[0:32], "0, PAWN")
-dump_string(scaled[32:64], "0, KNIGHT")
-dump_string(scaled[64:96], "0, BISHOP")
-dump_string(scaled[96:128], "0, ROOK")
-dump_string(scaled[128:160], "0, QUEEN")
-dump_string(scaled[160:192], "0, KING")
-dump_string(scaled[192:224], "1, PAWN")
-dump_string(scaled[224:256], "1, KNIGHT")
-dump_string(scaled[256:288], "1, BISHOP")
-dump_string(scaled[288:320], "1, ROOK")
-dump_string(scaled[320:352], "1, QUEEN")
-dump_string(scaled[352:384], "1, KING")
+dump_string(scaled[0:64], "0, PAWN")
+dump_string(scaled[64:128], "0, KNIGHT")
+dump_string(scaled[128:192], "0, BISHOP")
+dump_string(scaled[192:256], "0, ROOK")
+dump_string(scaled[256:320], "0, QUEEN")
+dump_string(scaled[320:384], "0, KING")
+dump_string(scaled[384:448], "1, PAWN")
+dump_string(scaled[448:512], "1, KNIGHT")
+dump_string(scaled[512:576], "1, BISHOP")
+dump_string(scaled[576:640], "1, ROOK")
+dump_string(scaled[640:704], "1, QUEEN")
+dump_string(scaled[704:768], "1, KING")
--- src/tables.cpp
+++ src/tables.cpp
@@ -5,10 +5,10 @@ void unpack(int phase, int piece, const char *data, double scale, int offset) {
     int16_t *white_section = PST[phase][piece | WHITE];
     int16_t *black_section = PST[phase][piece | BLACK];
     for (int rank = 0; rank < 80; rank+=10) {
-        for (int file = 0; file < 4; file++) {
+        for (int file = 0; file < 8; file++) {
             int v = (*data++ - ' ') * scale + offset;
-            white_section[rank+file] = white_section[rank+7-file] = v;
-            black_section[70-rank+file] = black_section[77-rank-file] = -v;
+            white_section[rank+file] = v;
+            black_section[70-rank+file] = -v;
         }
     }
 }
@@ -36,18 +36,18 @@ uint64_t ZOBRIST_STM;
 
 void init_tables() {
     // Piece-square tables
-    unpack(0, PAWN, " %*)8NK@<PHG;JLO=QQUCX`heSl~\")&!", 1.538, -8); // average: 50
-    unpack(0, KNIGHT, "BEGIFGNMIRSVTYZY][fc\\sw~WTps .%`", 1.73, 152); // average: 242
-    unpack(0, BISHOP, "FDBAQUQNQZXUXWYbZZho]tu~SZ_Z@1 .", 1.149, 203); // average: 263
-    unpack(0, ROOK, "/7;D ,04*6/2379=CIOVPdfs`[u{xx}~", 1.24, 264); // average: 319
-    unpack(0, QUEEN, " *(,048/572/631.G4:7ESDJH0G9@Qjh", 1.0, 663); // average: 691
-    unpack(0, KING, "RQB@JA6,.1# #+('(2142129A'4=~hkg", 2.052, -63); // average: -7
-    unpack(1, PAWN, " ## HJIIEHEEGJECNNJH]`YUz~vm\"!! ", 3.792, -5); // average: 142
-    unpack(1, KNIGHT, "(5IMAWX_L_fo[hvw`sx~\\grqOael etc", 1.487, 328); // average: 420
-    unpack(1, BISHOP, " /\"..:>>9ENQ4GTW8UPY>LRJ5NLNHMUV", 1.0, 439); // average: 475
-    unpack(1, ROOK, " //-'*02-29:<BECIOOJOJMFNUOQCGGH", 1.0, 736); // average: 767
-    unpack(1, QUEEN, "9& !2-)48?IGHU\\fKhmuUcwvSrq~[^UV", 1.639, 1437); // average: 1516
-    unpack(1, KING, " *+\".5=A:BIMDMRUP\\^^]pohX~um.\\[Z", 1.995, -84); // average: 8
+unpack(0, PAWN, "$!!&'$ !3EA=?NQ88FEBFFS<7DGKMKJ9<LJQSRO;AUYec`TAtlr~t[5N# &%'!!'", 1.602, -6); // average: 50
+unpack(0, KNIGHT, "FHKKLHIDFGNNORLJJRSWVUTMRWXXZ[[WXXac`f]^Yiks~zp]QTfrktU\\ /8Pl\"96", 2.074, 135); // average: 244
+unpack(0, BISHOP, "OPMLIKJOZ[XRUWZSW_\\\\Y\\]W\\Z_dd][\\X[gnlj_]Tjhxx~rfWY^[`ed[C;C1G AP", 1.538, 177); // average: 265
+unpack(0, ROOK, "-4<?C9:4!*03421 )0-.31:./17:=:<8<BIRVRPHIY[kwqoYXWm{wx_hqsy}|}|~", 1.258, 267); // average: 322
+unpack(0, QUEEN, "\")+-)%, .0513891040005;92//-345:9/45<>9E8B:HL[VOB*;9<UDN3I`dl~XJ", 1.021, 671); // average: 699
+unpack(0, KING, "BGC5;6JLD=4++4=C02%#\"%/-+-+)''*!/4/5/0.$550841*,D06<5. 4~ejbSQR[", 2.548, -68); // average: -7
+unpack(1, PAWN, "!#\"#\"!\" IJIJIIJGFHDEEFGDHJECDEJFOOKHHIML_a[UVW`Zy}vlmv~y#! # ! #", 3.803, -5); // average: 142
+unpack(1, KNIGHT, "*3GMMJ:'@XX__VUBJ]enne`NZguuwuh[]ovz~xtb[gstloh\\O`fjla`O fpf]td&", 1.519, 326); // average: 419
+unpack(1, BISHOP, "*6 6635\"9?IEEAA2@OWXWRH@=O\\^\\YN9>[T_aZ[CIQZORUWD:WSYRSQ<UUX^Z`SI", 1.0, 431); // average: 474
+unpack(1, ROOK, "/:::553 37==;3,,9@EEA?40JQROJJD>V^\\VQRPL^Y[SKNKP\\b]ZYQXPNSQQQOMH", 1.0, 727); // average: 767
+unpack(1, QUEEN, "@2*'*\" 58=5:6-%7@DOILJD;EW[ja_WSLclrvpqZReptvwpbOotz~oua\\`YYWSdd", 1.795, 1420); // average: 1513
+unpack(1, KING, "+./*%-+ 08>BB=6/:BIMMIA;CLRUTRMEMZ][]]\\QYjlegmo^Svqikr~[-UWWZ]`4", 2.156, -90); // average: 9
 
     // Zobrist keys
 #ifdef OPENBENCH
