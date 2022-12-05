3d4a8c617f8b0fe0c0b4861fcc2c252e27de1b9b
MinusKelvin <mark.carlson@minuskelvin.net>
2022-12-05 11:23:34 +1100

    compile for native arch
    size: 3530
    bench: 7945664

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ if [ -z "$1" ]; then
     chmod +x ice4
     echo Compressed size: `du -b ice4`
 else
-    g++ -DOPENBENCH -O3 -pthread src/main.cpp -o "$1"
+    g++ -march=native -DOPENBENCH -O3 -pthread src/main.cpp -o "$1"
 fi
