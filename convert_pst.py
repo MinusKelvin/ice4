360e1ba4fa17da2565643c372000363a61d0a734
MinusKelvin <mark.carlson@minuskelvin.net>
2022-12-06 19:39:30 +1100

    outer product psts, k=3
    size: 3380
    bench: 8060362

--- convert_pst.py
+++ convert_pst.py
@@ -1,38 +1,68 @@
 #!/usr/bin/python3
 
 import json, math
+from scipy.optimize import least_squares
+import numpy as np
+
 with open("0-45.json", "r") as f:
     data = json.load(f)
 
-def dump_string(piece_data, stuff):
-    smallest = float("inf")
-    largest = -smallest
-    average = 0
-    for value in piece_data:
-        smallest = smallest if smallest < value else value
-        largest = largest if largest > value else value
-        average += value / len(piece_data)
+def quantize(vector):
+    smallest = vector.min()
+    largest = vector.max()
+    scale_factor = 94 / (largest - smallest)
+    values = np.around((vector - smallest) * scale_factor)
+    unquantized = values / scale_factor + smallest
+    return smallest, scale_factor, values, unquantized
 
-    scale_factor = max((largest - smallest) / 94, 1.0)
-    print(f"unpack({stuff}, \"", end="")
-    for value in piece_data:
-        c = chr(round((value - smallest) / scale_factor) + 32)
-        if c == "\\" or c == "\"":
+groups = []
+for i in range(12):
+    values = np.array(data["pst.weight"][i*64:i*64+64]).ravel() * -160
+    if i == 0 or i == 6:
+        values[0:8] = np.median(values)
+        values[56:64] = np.median(values)
+    values = np.around(values.reshape((8,8)))
+    print(int(np.amin(values)), end=", ")
+    values -= np.amin(values)
+    groups.append(values)
+print()
+
+def model(x, group):
+    files = x[0:48]
+    ranks = x[48:64]
+    pst = np.outer(files, ranks).ravel()
+    residuals = group - pst
+    return residuals
+
+pawns = np.concatenate((groups[0], groups[6]), axis=1)
+knights = np.concatenate((groups[1], groups[7]), axis=1)
+bishops = np.concatenate((groups[2], groups[8]), axis=1)
+rooks = np.concatenate((groups[3], groups[9]), axis=1)
+queens = np.concatenate((groups[4], groups[10]), axis=1)
+kings = np.concatenate((groups[5], groups[11]), axis=1)
+
+target = np.concatenate((pawns, knights, bishops, rooks, queens, kings), axis=0).ravel()
+remaining = target
+predicted = np.zeros_like(remaining)
+rng = np.random.default_rng()
+
+ITERS = 3
+
+for i in range(ITERS):
+    result = least_squares(model, rng.uniform(-1, 1, 64), args=(remaining,))
+    smallest, scale, quant, unquant = quantize(result.x)
+
+    print(f"unpack({smallest:.4}, {1/scale:.4}, \"", end="")
+    for v in quant:
+        c = chr(int(v) + 32)
+        if c == "\"" or c == "\\":
             print("\\", end="")
         print(c, end="")
-    print(f"\", {scale_factor:.4}, {round(smallest)}); // average: {average:.0f}")
+    print("\");", end="")
 
-scaled = [v[0] * -160 for v in data["pst.weight"]]
+    prediction = np.outer(unquant[0:48], unquant[48:64]).ravel()
+    predicted += prediction
+    predicted = np.trunc(predicted)
+    remaining = target - predicted
 
-dump_string(scaled[0:64], "0, PAWN")
-dump_string(scaled[64:128], "0, KNIGHT")
-dump_string(scaled[128:192], "0, BISHOP")
-dump_string(scaled[192:256], "0, ROOK")
-dump_string(scaled[256:320], "0, QUEEN")
-dump_string(scaled[320:384], "0, KING")
-dump_string(scaled[384:448], "1, PAWN")
-dump_string(scaled[448:512], "1, KNIGHT")
-dump_string(scaled[512:576], "1, BISHOP")
-dump_string(scaled[576:640], "1, ROOK")
-dump_string(scaled[640:704], "1, QUEEN")
-dump_string(scaled[704:768], "1, KING")
+    print(f" // mse = {np.mean(np.square(remaining)):.4}")
