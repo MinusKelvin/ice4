7db6254e01c801ca7524bcface4a94e335b2df47
MinusKelvin <mark.carlson@minuskelvin.net>
2022-12-17 11:55:23 +1100

    sparse, factorized 16-neuron nnue
    size: 4377
    bench: 5586559

--- convert_pst.py
+++ convert_pst.py
@@ -4,20 +4,22 @@ import json, math
 from scipy.optimize import least_squares
 import numpy as np
 
-with open("0-45.json", "r") as f:
+with open("0-42.json", "r") as f:
     data = json.load(f)
 
 def quantize(vector):
     smallest = vector.min()
     largest = vector.max()
-    scale_factor = 94 / (largest - smallest)
+    scale_factor = 94 / (largest - smallest) if largest > smallest else 1
     values = np.around((vector - smallest) * scale_factor)
     unquantized = values / scale_factor + smallest
     return smallest, scale_factor, values, unquantized
 
-for i, v in enumerate(data["ft.weight"]):
-    v = np.array(v) * 256
-    smallest, scale, quant, unquant = quantize(v)
+for i, (v, vg, f, fg) in enumerate(zip(data["ft.weight"], data["ft.gate"], data["material_fact.weight"], data["material_fact.gate"])):
+    v = np.array(v) * (np.array(vg) > 0.5)
+    f = (np.array(f) * (np.array(fg) > 0.5)).reshape((12, 1))
+    v = v + np.pad(f, ((0, 0), (0, 63)), "edge").ravel()
+    smallest, scale, quant, unquant = quantize(v * 256)
 
     print(f"unpack({i}, {smallest:.4}, {1/scale:.4}, \"", end="")
     for v in quant:
