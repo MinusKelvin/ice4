aff30bdbb47e462669ca0c741e45f0ff9377cef9
MinusKelvin <mark.carlson@minuskelvin.net>
2022-12-30 23:59:09 +1100

    randomly disable NMP occasionally
    size: 4101
    bench: 11402387

--- src/search.cpp
+++ src/search.cpp
@@ -51,7 +51,7 @@ struct Searcher {
             return static_eval;
         }
 
-        if (!pv && static_eval >= beta && depth > 1) {
+        if (!pv && static_eval >= beta && depth > 1 && !(nodes & 0x3FF)) {
             Board mkmove = board;
             mkmove.null_move();
 
