5f52044ce1153cc09993958d68158eac5f7a7882
MinusKelvin <mark.carlson@minuskelvin.net>
2023-01-04 13:07:43 +1100

    don't do nmp if losing mate
    size: 4046
    bench: 6682164

--- src/search.cpp
+++ src/search.cpp
@@ -51,7 +51,7 @@ struct Searcher {
             return static_eval;
         }
 
-        if (!pv && static_eval >= beta && depth > 1 && depth < 50) {
+        if (!pv && static_eval >= beta && beta > -20000 && depth > 1) {
             Board mkmove = board;
             mkmove.null_move();
 
