61c3e4cceb8812379a729bde5742e09674fde338
MinusKelvin <mark.carlson@minuskelvin.net>
2023-01-23 10:05:32 +1100

    adjust lmr
    size: 4094
    bench: 5993424

--- src/search.cpp
+++ src/search.cpp
@@ -144,7 +144,7 @@ struct Searcher {
             if (is_rep) {
                 v = 0;
             } else if (legals) {
-                int reduction = (legals*3 + depth*2) / 32;
+                int reduction = legals * 0.1 + depth * 0.06;
                 if (reduction > legals) {
                     reduction = legals;
                 }
