33ac39d034b782631cb1907949353891d5ab7b77
MinusKelvin <mark.carlson@minuskelvin.net>
2023-01-26 08:54:22 +1100

    use improving in lmr
    size: 4081
    bench: 5597595

--- src/search.cpp
+++ src/search.cpp
@@ -147,7 +147,7 @@ struct Searcher {
             if (is_rep) {
                 v = 0;
             } else if (legals) {
-                int reduction = (legals*3 + depth*2) / 32;
+                int reduction = (legals*3 + depth*2 + (improving ? -16 : 16)) / 32;
                 if (reduction > legals) {
                     reduction = legals;
                 }
