d8859a646a92120d0a0e045ac7feac1e909ce547
MinusKelvin <mark.carlson@minuskelvin.net>
2023-01-25 14:51:00 +1100

    don't limit to 99 quiets
    size: 4096
    bench: 6194793

--- src/search.cpp
+++ src/search.cpp
@@ -106,7 +106,7 @@ struct Searcher {
         }
 
         int quiets_to_check_table[] = { 0, 7, 8, 17 };
-        int quiets_to_check = depth > 0 && depth < 4 && !pv ? quiets_to_check_table[depth] : 99;
+        int quiets_to_check = depth > 0 && depth < 4 && !pv ? quiets_to_check_table[depth] : -1;
 
         int legals = 0;
         for (int i = 0; i < mvcount; i++) {
