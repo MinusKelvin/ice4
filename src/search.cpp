97a4823f89580a2cdc7c8e8b00d7162168621f5f
MinusKelvin <mark.carlson@minuskelvin.net>
2023-01-31 08:46:07 +1100

    base lmr on pseudolegal count
    size: 4088
    bench: 5466095

--- src/search.cpp
+++ src/search.cpp
@@ -135,7 +135,7 @@ struct Searcher {
                 if (is_rep) {
                     v = 0;
                 } else if (legals) {
-                    int reduction = (legals*3 + depth*2) / 32;
+                    int reduction = (i*3 + depth*2) / 32;
                     if (reduction > legals) {
                         reduction = legals;
                     }
