be1983b9622e63889203e97bd724a94ff0482d4f
MinusKelvin <mark.carlson@minuskelvin.net>
2023-01-30 14:02:55 +1100

    return immediately when lmp triggers
    size: 4053
    bench: 4777395

--- src/search.cpp
+++ src/search.cpp
@@ -111,7 +111,7 @@ struct Searcher {
                 swap(score[i], score[best_so_far]);
 
                 if (!(quiets_to_check -= !board.board[moves[i].to])) {
-                    break;
+                    return legals ? best : alpha;
                 }
 
                 Board mkmove = board;
