778ac1e1f69e6198c56d1dad4e42e08ccfe7c5c3
MinusKelvin <mark.carlson@minuskelvin.net>
2023-03-03 09:45:23 +1100

    conthist considers previous move is capture
    size: 4101
    bench: 3987737

--- src/search.cpp
+++ src/search.cpp
@@ -18,7 +18,7 @@ struct Searcher {
     double abort_time;
     int16_t evals[256];
     HTable history;
-    HTable conthist[14][SQUARE_SPAN];
+    HTable conthist[2][14][SQUARE_SPAN];
     HTable *conthist_stack[256];
     uint64_t rep_list[256];
 
@@ -71,7 +71,7 @@ struct Searcher {
         if (!pv && eval >= beta && beta > -20000 && depth > 1) {
             Board mkmove = board;
             mkmove.null_move();
-            conthist_stack[ply] = &conthist[0][0];
+            conthist_stack[ply] = &conthist[0][0][0];
 
             int reduction = (eval - beta) / 128 + depth / 3 + 2;
 
@@ -135,7 +135,7 @@ struct Searcher {
 
                 Board mkmove = board;
                 mkmove.make_move(moves[i]);
-                conthist_stack[ply] = &conthist[board.board[moves[i].from] - WHITE_PAWN][moves[i].to-A1];
+                conthist_stack[ply] = &conthist[!victim][board.board[moves[i].from] - WHITE_PAWN][moves[i].to-A1];
                 if (!(++nodes & 0xFFF) && (ABORT || now() > abort_time)) {
                     throw 0;
                 }
