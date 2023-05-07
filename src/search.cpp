faa88a8daadf389b88e3fa2ded9e766d572f00ff
MinusKelvin <mark.carlson@minuskelvin.net>
2023-05-07 10:56:29 +1000

    tt pv cut larger requirement
    size: 4089
    bench: 4186867

--- src/search.cpp
+++ src/search.cpp
@@ -45,7 +45,7 @@ struct Searcher {
             if (depth > 0 || board.board[tt.mv.to]) {
                 hashmv = tt.mv;
             }
-            if (depth <= tt.depth && (
+            if (depth + 3*pv <= tt.depth && (
                 tt.bound == BOUND_EXACT ||
                 tt.bound == BOUND_LOWER && tt.eval >= beta ||
                 tt.bound == BOUND_UPPER && tt.eval <= alpha
