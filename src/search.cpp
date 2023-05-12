0131ab45c2fc9c8429c3d052301b257db498ecd3
MinusKelvin <mark.carlson@minuskelvin.net>
2023-05-12 18:15:47 +1000

    remove staged movegen
    size: 3968
    bench: 4703050

--- src/search.cpp
+++ src/search.cpp
@@ -97,6 +97,43 @@ struct Searcher {
             in_check = !mkmove.movegen(moves, mvcount);
         }
 
+        if (!board.movegen(moves, mvcount, depth > 0)) {
+            return WON;
+        }
+
+        for (int j = 0; j < mvcount; j++) {
+            if (hashmv == moves[j]) {
+                score[j] = 30000;
+            } else if (board.board[moves[j].to]) {
+                // MVV-LVA capture ordering: 3 bytes (78a3963 vs 35f9b66)
+                // 8.0+0.08: 289.03 +- 7.40 (7378 - 563 - 2059) 96.34 elo/byte
+                // 60.0+0.6: 237.53 +- 6.10 (6384 - 445 - 3171) 79.18 elo/byte
+                score[j] = (board.board[moves[j].to] & 7) * 8
+                    - (board.board[moves[j].from] & 7)
+                    + 20000;
+            } else {
+                // Plain history: 28 bytes (676e7fa vs 4cabdf1)
+                // 8.0+0.08: 51.98 +- 5.13 (3566 - 2081 - 4353) 1.86 elo/byte
+                // 60.0+0.6: 52.37 +- 4.62 (3057 - 1561 - 5382) 1.87 elo/byte
+                score[j] = history[board.board[moves[j].from] - WHITE_PAWN][moves[j].to-A1]
+                    // Continuation histories: 87 bytes (af63703 vs 4cabdf1)
+                    // 8.0+0.08: 22.93 +- 5.09 (3124 - 2465 - 4411) 0.26 elo/byte
+                    // 60.0+0.6: 46.52 +- 4.57 (2930 - 1599 - 5471) 0.53 elo/byte
+                    // Countermove history: 21 bytes (42a57f7 vs 4cabdf1)
+                    // 8.0+0.08: 17.98 +- 5.12 (3084 - 2567 - 4349) 0.86 elo/byte
+                    // 60.0+0.6: 21.64 +- 4.51 (2508 - 1886 - 5606) 1.03 elo/byte
+                    + (ply ?
+                        (*conthist_stack[ply - 1])[board.board[moves[j].from] - WHITE_PAWN][moves[j].to-A1]
+                    : 0)
+                    // Followup history: 22 bytes (ae6f9fa vs 4cabdf1)
+                    // 8.0+0.08: 9.07 +- 5.06 (2893 - 2632 - 4475) 0.41 elo/byte
+                    // 60.0+0.6: 13.42 +- 4.52 (2396 - 2010 - 5594) 0.61 elo/byte
+                    + (ply > 1 ?
+                        (*conthist_stack[ply - 2])[board.board[moves[j].from] - WHITE_PAWN][moves[j].to-A1]
+                    : 0);
+            }
+        }
+
         // Internal Iterative Deepening: 24 bytes (bd674e0 vs 98a56ea)
         // 8.0+0.08: 67.08 +- 5.38 (4027 - 2120 - 3853) 2.80 elo/byte
         // 60.0+0.6: 94.47 +- 4.95 (3952 - 1298 - 4750) 3.94 elo/byte
@@ -105,9 +142,6 @@ struct Searcher {
         }
 
         rep_list[ply] = board.zobrist;
-        moves[0] = hashmv;
-        score[0] = 0;
-        mvcount = 1;
 
         int best = depth > 0 ? LOST + ply : eval;
         if (best >= beta) {
@@ -120,172 +154,125 @@ struct Searcher {
         int raised_alpha = 0;
         int legals = 0;
         for (int i = 0; i < mvcount; i++) {
-            if (moves[i].from) {
-                int best_so_far = i;
-                for (int j = i+1; j < mvcount; j++) {
-                    if (score[j] > score[best_so_far]) {
-                        best_so_far = j;
-                    }
-                }
-                swap(moves[i], moves[best_so_far]);
-                swap(score[i], score[best_so_far]);
-
-                int victim = board.board[moves[i].to] & 7;
-                int deltas[] = {1350, 210, 390, 440, 680, 1350, 0};
-
-                // Late Move Pruning (incl. improving): 66 bytes (ee0073a vs b5fdb00)
-                // 8.0+0.08: 101.80 +- 5.40 (4464 - 1615 - 3921) 1.54 elo/byte
-                // 60.0+0.6: 97.13 +- 4.79 (3843 - 1118 - 5039) 1.47 elo/byte
-                if (!(quiets_to_check -= !victim)) {
-                    break;
-                }
-
-                // Delta Pruning: 37 bytes (939b3de vs 4cabdf1)
-                // 8.0+0.08: 25.30 +- 5.11 (3175 - 2448 - 4377) 0.68 elo/byte
-                // 60.0+0.6: 21.67 +- 4.55 (2551 - 1928 - 5521) 0.59 elo/byte
-                if (depth <= 0 && eval + deltas[victim] <= alpha) {
-                    continue;
-                }
-
-                Board mkmove = board;
-                mkmove.make_move(moves[i]);
-                conthist_stack[ply] = &conthist[board.board[moves[i].from] - WHITE_PAWN][moves[i].to-A1];
-                if (!(++nodes & 0xFFF) && (ABORT || now() > abort_time)) {
-                    throw 0;
-                }
-
-                int is_rep = 0;
-                for (int i = ply-1; depth > 0 && !is_rep && i >= 0; i -= 2) {
-                    is_rep |= rep_list[i] == mkmove.zobrist;
-                }
-                for (int i = 0; depth > 0 && !is_rep && i < PREHISTORY_LENGTH; i++) {
-                    is_rep |= PREHISTORY[i] == mkmove.zobrist;
-                }
-
-                int v;
-
-                if (is_rep) {
-                    v = 0;
-                } else if (legals) {
-                    // All reductions: 57 bytes (a8e89fa vs 98a56ea)
-                    // 8.0+0.08: 181.21 +- 6.27 (6020 - 1231 - 2749) 3.18 elo/byte
-                    // 60.0+0.6: 179.68 +- 5.89 (5716 - 961 - 3323) 3.15 elo/byte
-                    int reduction = (legals*3 + depth*4) / 40;
-                    // Extra reduction condition: 5 bytes (e61a8aa vs 0e2f650)
-                    // 8.0+0.08: 22.65 +- 5.17 (3207 - 2556 - 4237) 4.53 elo/byte
-                    // 60.0+0.6: 14.32 +- 4.67 (2557 - 2145 - 5298) 2.86 elo/byte
-                    reduction += legals > 4;
-                    // History Reduction: 6 bytes (bf488d7 vs 0e2f650)
-                    // 8.0+0.08: 17.60 +- 5.06 (3011 - 2505 - 4484) 2.93 elo/byte
-                    // 60.0+0.6: 48.01 +- 4.69 (3062 - 1689 - 5249) 8.00 elo/byte
-                    reduction -= score[i] / 346;
-                    if (reduction < 0 || victim || in_check) {
-                        reduction = 0;
-                    }
-                    v = -negamax(mkmove, scratch, -alpha-1, -alpha, depth - reduction - 1, ply + 1);
-                    if (v > alpha && reduction) {
-                        // reduced search failed high, re-search at full depth
-                        v = -negamax(mkmove, scratch, -alpha-1, -alpha, depth - 1, ply + 1);
-                    }
-                    if (v > alpha && v < beta) {
-                        // at pv nodes, we need to re-search with full window when move raises alpha
-                        // at non-pv nodes, this would be equivalent to the previous search, so skip it
-                        v = -negamax(mkmove, scratch, -beta, -alpha, depth - 1, ply + 1);
-                    }
-                } else {
-                    // first legal move is always searched with full window
-                    v = -negamax(mkmove, scratch, -beta, -alpha, depth - 1 + in_check, ply + 1);
-                }
-                if (v == LOST) {
-                    moves[i].from = 1;
-                } else {
-                    legals++;
-                }
-                if (v > best) {
-                    best = v;
-                    bestmv = moves[i];
-                }
-                if (v > alpha) {
-                    alpha = v;
-                    raised_alpha = 1;
-                }
-                if (v >= beta) {
-                    if (!victim) {
-                        int change = depth * depth;
-                        int16_t *hist;
-                        for (int j = 0; j < i; j++) {
-                            if (board.board[moves[j].to]) {
-                                continue;
-                            }
-                            hist = &history[board.board[moves[j].from] - WHITE_PAWN][moves[j].to-A1];
-                            *hist -= change + change * *hist / MAX_HIST;
-                            if (ply) {
-                                hist = &(*conthist_stack[ply - 1])[board.board[moves[j].from] - WHITE_PAWN][moves[j].to-A1];
-                                *hist -= change + change * *hist / MAX_HIST;
-                            }
-                            if (ply > 1) {
-                                hist = &(*conthist_stack[ply - 2])[board.board[moves[j].from] - WHITE_PAWN][moves[j].to-A1];
-                                *hist -= change + change * *hist / MAX_HIST;
-                            }
-                        }
-                        hist = &history[board.board[moves[i].from] - WHITE_PAWN][moves[i].to-A1];
-                        *hist += change - change * *hist / MAX_HIST;
-                        if (ply) {
-                            hist = &(*conthist_stack[ply - 1])[board.board[moves[i].from] - WHITE_PAWN][moves[i].to-A1];
-                            *hist += change - change * *hist / MAX_HIST;
-                        }
-                        if (ply > 1) {
-                            hist = &(*conthist_stack[ply - 2])[board.board[moves[i].from] - WHITE_PAWN][moves[i].to-A1];
-                            *hist += change - change * *hist / MAX_HIST;
-                        }
-                    }
-                    break;
+            int best_so_far = i;
+            for (int j = i+1; j < mvcount; j++) {
+                if (score[j] > score[best_so_far]) {
+                    best_so_far = j;
                 }
             }
+            swap(moves[i], moves[best_so_far]);
+            swap(score[i], score[best_so_far]);
 
-            // cases that reach this point with i == 0:
-            // 1. hashmv does not exist, moves[0] does not exist => movegen
-            // 2. hashmv does not exist, moves[0] exists => already did movegen
-            // 3. hashmv exists (implies moves[0] exists) => movegen
-            if (!i && (!moves[0].from || hashmv.from)) {
-                if (!board.movegen(moves, mvcount, depth > 0)) {
-                    return WON;
+            int victim = board.board[moves[i].to] & 7;
+            int deltas[] = {1350, 210, 390, 440, 680, 1350, 0};
+
+            // Late Move Pruning (incl. improving): 66 bytes (ee0073a vs b5fdb00)
+            // 8.0+0.08: 101.80 +- 5.40 (4464 - 1615 - 3921) 1.54 elo/byte
+            // 60.0+0.6: 97.13 +- 4.79 (3843 - 1118 - 5039) 1.47 elo/byte
+            if (!(quiets_to_check -= !victim)) {
+                break;
+            }
+
+            // Delta Pruning: 37 bytes (939b3de vs 4cabdf1)
+            // 8.0+0.08: 25.30 +- 5.11 (3175 - 2448 - 4377) 0.68 elo/byte
+            // 60.0+0.6: 21.67 +- 4.55 (2551 - 1928 - 5521) 0.59 elo/byte
+            if (depth <= 0 && eval + deltas[victim] <= alpha) {
+                continue;
+            }
+
+            Board mkmove = board;
+            mkmove.make_move(moves[i]);
+            conthist_stack[ply] = &conthist[board.board[moves[i].from] - WHITE_PAWN][moves[i].to-A1];
+            if (!(++nodes & 0xFFF) && (ABORT || now() > abort_time)) {
+                throw 0;
+            }
+
+            int is_rep = 0;
+            for (int i = ply-1; depth > 0 && !is_rep && i >= 0; i -= 2) {
+                is_rep |= rep_list[i] == mkmove.zobrist;
+            }
+            for (int i = 0; depth > 0 && !is_rep && i < PREHISTORY_LENGTH; i++) {
+                is_rep |= PREHISTORY[i] == mkmove.zobrist;
+            }
+
+            int v;
+
+            if (is_rep) {
+                v = 0;
+            } else if (legals) {
+                // All reductions: 57 bytes (a8e89fa vs 98a56ea)
+                // 8.0+0.08: 181.21 +- 6.27 (6020 - 1231 - 2749) 3.18 elo/byte
+                // 60.0+0.6: 179.68 +- 5.89 (5716 - 961 - 3323) 3.15 elo/byte
+                int reduction = (legals*3 + depth*4) / 40;
+                // Extra reduction condition: 5 bytes (e61a8aa vs 0e2f650)
+                // 8.0+0.08: 22.65 +- 5.17 (3207 - 2556 - 4237) 4.53 elo/byte
+                // 60.0+0.6: 14.32 +- 4.67 (2557 - 2145 - 5298) 2.86 elo/byte
+                reduction += legals > 4;
+                // History Reduction: 6 bytes (bf488d7 vs 0e2f650)
+                // 8.0+0.08: 17.60 +- 5.06 (3011 - 2505 - 4484) 2.93 elo/byte
+                // 60.0+0.6: 48.01 +- 4.69 (3062 - 1689 - 5249) 8.00 elo/byte
+                reduction -= score[i] / 346;
+                if (reduction < 0 || victim || in_check) {
+                    reduction = 0;
                 }
-                for (int j = 0; j < mvcount; j++) {
-                    if (hashmv == moves[j]) {
-                        swap(moves[0], moves[j]);
-                        swap(score[0], score[j]);
-                    } else if (board.board[moves[j].to]) {
-                        // MVV-LVA capture ordering: 3 bytes (78a3963 vs 35f9b66)
-                        // 8.0+0.08: 289.03 +- 7.40 (7378 - 563 - 2059) 96.34 elo/byte
-                        // 60.0+0.6: 237.53 +- 6.10 (6384 - 445 - 3171) 79.18 elo/byte
-                        score[j] = (board.board[moves[j].to] & 7) * 8
-                            - (board.board[moves[j].from] & 7)
-                            + 20000;
-                    } else {
-                        // Plain history: 28 bytes (676e7fa vs 4cabdf1)
-                        // 8.0+0.08: 51.98 +- 5.13 (3566 - 2081 - 4353) 1.86 elo/byte
-                        // 60.0+0.6: 52.37 +- 4.62 (3057 - 1561 - 5382) 1.87 elo/byte
-                        score[j] = history[board.board[moves[j].from] - WHITE_PAWN][moves[j].to-A1]
-                            // Continuation histories: 87 bytes (af63703 vs 4cabdf1)
-                            // 8.0+0.08: 22.93 +- 5.09 (3124 - 2465 - 4411) 0.26 elo/byte
-                            // 60.0+0.6: 46.52 +- 4.57 (2930 - 1599 - 5471) 0.53 elo/byte
-                            // Countermove history: 21 bytes (42a57f7 vs 4cabdf1)
-                            // 8.0+0.08: 17.98 +- 5.12 (3084 - 2567 - 4349) 0.86 elo/byte
-                            // 60.0+0.6: 21.64 +- 4.51 (2508 - 1886 - 5606) 1.03 elo/byte
-                            + (ply ?
-                                (*conthist_stack[ply - 1])[board.board[moves[j].from] - WHITE_PAWN][moves[j].to-A1]
-                            : 0)
-                            // Followup history: 22 bytes (ae6f9fa vs 4cabdf1)
-                            // 8.0+0.08: 9.07 +- 5.06 (2893 - 2632 - 4475) 0.41 elo/byte
-                            // 60.0+0.6: 13.42 +- 4.52 (2396 - 2010 - 5594) 0.61 elo/byte
-                            + (ply > 1 ?
-                                (*conthist_stack[ply - 2])[board.board[moves[j].from] - WHITE_PAWN][moves[j].to-A1]
-                            : 0);
+                v = -negamax(mkmove, scratch, -alpha-1, -alpha, depth - reduction - 1, ply + 1);
+                if (v > alpha && reduction) {
+                    // reduced search failed high, re-search at full depth
+                    v = -negamax(mkmove, scratch, -alpha-1, -alpha, depth - 1, ply + 1);
+                }
+                if (v > alpha && v < beta) {
+                    // at pv nodes, we need to re-search with full window when move raises alpha
+                    // at non-pv nodes, this would be equivalent to the previous search, so skip it
+                    v = -negamax(mkmove, scratch, -beta, -alpha, depth - 1, ply + 1);
+                }
+            } else {
+                // first legal move is always searched with full window
+                v = -negamax(mkmove, scratch, -beta, -alpha, depth - 1 + in_check, ply + 1);
+            }
+            if (v == LOST) {
+                moves[i].from = 1;
+            } else {
+                legals++;
+            }
+            if (v > best) {
+                best = v;
+                bestmv = moves[i];
+            }
+            if (v > alpha) {
+                alpha = v;
+                raised_alpha = 1;
+            }
+            if (v >= beta) {
+                if (!victim) {
+                    int change = depth * depth;
+                    int16_t *hist;
+                    for (int j = 0; j < i; j++) {
+                        if (board.board[moves[j].to]) {
+                            continue;
+                        }
+                        hist = &history[board.board[moves[j].from] - WHITE_PAWN][moves[j].to-A1];
+                        *hist -= change + change * *hist / MAX_HIST;
+                        if (ply) {
+                            hist = &(*conthist_stack[ply - 1])[board.board[moves[j].from] - WHITE_PAWN][moves[j].to-A1];
+                            *hist -= change + change * *hist / MAX_HIST;
+                        }
+                        if (ply > 1) {
+                            hist = &(*conthist_stack[ply - 2])[board.board[moves[j].from] - WHITE_PAWN][moves[j].to-A1];
+                            *hist -= change + change * *hist / MAX_HIST;
+                        }
+                    }
+                    hist = &history[board.board[moves[i].from] - WHITE_PAWN][moves[i].to-A1];
+                    *hist += change - change * *hist / MAX_HIST;
+                    if (ply) {
+                        hist = &(*conthist_stack[ply - 1])[board.board[moves[i].from] - WHITE_PAWN][moves[i].to-A1];
+                        *hist += change - change * *hist / MAX_HIST;
+                    }
+                    if (ply > 1) {
+                        hist = &(*conthist_stack[ply - 2])[board.board[moves[i].from] - WHITE_PAWN][moves[i].to-A1];
+                        *hist += change - change * *hist / MAX_HIST;
                     }
                 }
-                // need to step back loop variable in case 1
-                i -= !hashmv.from;
+                break;
             }
         }
 
