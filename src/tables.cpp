29b808a3df519caf2fc0c654e5e8407794ebae1f
MinusKelvin <mark.carlson@minuskelvin.net>
2023-05-14 08:23:44 +1000

    full linear mobility terms
    size: 4086
    bench: 4795017

--- src/tables.cpp
+++ src/tables.cpp
@@ -1,27 +1,33 @@
 int16_t PST[2][25][SQUARE_SPAN];
-int16_t DOUBLED_MG[] = {8, -8, 16, 18, 19, 14, -4, 6};
-int16_t DOUBLED_EG[] = {30, 22, 15, 10, 8, 17, 21, 35};
-int16_t PROTECTED_PAWN_MG[] = {0, 7, 9};
+int16_t DOUBLED_MG[] = {7, -8, 15, 17, 18, 13, -4, 6};
+int16_t DOUBLED_EG[] = {29, 22, 15, 10, 8, 16, 21, 34};
+int16_t PROTECTED_PAWN_MG[] = {0, 7, 8};
 int16_t PROTECTED_PAWN_EG[] = {0, 7, 4};
-int16_t PAWN_SHIELD_MG[] = {10, 19, 20, 25};
-int16_t PAWN_SHIELD_EG[] = {-25, -37, -25, -14};
+int16_t PAWN_SHIELD_MG[] = {0, 8, 10, 13};
+int16_t PAWN_SHIELD_EG[] = {-19, -31, -19, -8};
+int16_t MOBILITY_MG[] = {0, 1, 1, 2, 1, 0, -5};
+int16_t MOBILITY_EG[] = {0, 1, -5, 2, 1, 2, 2};
 int PHASE[] = {0, 0, 1, 1, 2, 4, 0};
-#define BISHOP_PAIR_MG 23
-#define BISHOP_PAIR_EG 41
+int RAYS[] = {-1, 1, -10, 10, 11, -11, 9, -9, -21, 21, -19, 19, -12, 12, -8, 8};
+int STARTS[] = {0,0,8,4,0,0,0};
+int LIMITS[] = {0,0,1,8,8,8,1};
+int ENDS[] = {0,0,16,8,4,8,8};
+#define BISHOP_PAIR_MG 22
+#define BISHOP_PAIR_EG 43
 #define TEMPO_MG 6
 #define TEMPO_EG 2
 #define ISOLATED_PAWN_MG 8
-#define ISOLATED_PAWN_EG 11
-#define ROOK_OPEN_MG 29
-#define ROOK_OPEN_EG 11
-#define ROOK_SEMIOPEN_MG 16
-#define ROOK_SEMIOPEN_EG 16
-#define KING_OPEN_MG -30
-#define KING_OPEN_EG -5
-#define KING_SEMIOPEN_MG -9
-#define KING_SEMIOPEN_EG 19
+#define ISOLATED_PAWN_EG 10
+#define ROOK_OPEN_MG 27
+#define ROOK_OPEN_EG 8
+#define ROOK_SEMIOPEN_MG 15
+#define ROOK_SEMIOPEN_EG 15
+#define KING_OPEN_MG -29
+#define KING_OPEN_EG -6
+#define KING_SEMIOPEN_MG -8
+#define KING_SEMIOPEN_EG 18
 
-const char *DATA_STRING = "EY\\@<A93HVNFCD98HO[XTM=;OXgc`VKE[c{vqgVK@ i~vl7h%(0642,.! ,,,('%%)'&%(,(-/.+-0216C9??=>:aHn~yurz.932.+*0,-%&  +/.-#!$&46-6/025C=3*63<DKQSNAGW\\~V#$  ##+\"'-'%$&*'694/+,13EF>6038?XUO;6<GPe~Z5/5DBO>@_6 \"4KPG7wv[C&2. 5LI2VbcTVhg[ '),.26245423-209.% -/,4,CNK2MWc139? )21*512('01 &&\"()&($%(''.,)#  !-73..<8611:D 0&-+3872=EG,AGJ #).)-94+?=E;8HG BUXL\\biVirx_qz~";
+const char *DATA_STRING = ">OQ96:5/ANG@=>44BHROLE86HO\\XUMC>QXmje\\MD@ d~yn<j%*3;85.0! ...)(%%+)''*.*/21-0365:K?FFBC?gTt~tvzr/;651,+2-.%(  ,0/.# #&78.70026G@6*84=HPXRO::KT~M$$  ##,#(-(%$&+'6:50,-24FG?7149@ZVP<7=IQi~`A=?KRM<>\\2 !0EOF0sv\\>&2. 6KH4XbbUWgf\\ '(*,/2.221.0*.,8,\" /-*2/BJG4LT]138? )11*502('01 %% **')&&)(*0.+%   +3-(,84//,3= .$+*04309>A*<@A $(-(,62+=;C:6ED ?QTGWbgRgtyYm{~";
 
 void unpack_full(int phase, int piece, double scale, int offset) {
     int16_t *white_section = PST[phase][piece | WHITE];
@@ -93,20 +99,20 @@ struct Zobrist {
 } ZOBRIST;
 
 void init_tables() {
-    unpack_full(0, PAWN, 1.088, 10); // average: 64
-    unpack_full(1, PAWN, 1.337, 93); // average: 126
-    unpack_full(0, PASSED_PAWN, 1.251, -14); // average: 14
-    unpack_full(1, PASSED_PAWN, 1.979, -5); // average: 40
-    unpack_smol(0, KING, 1.0, -38); // average: 2
-    unpack_smol(1, KING, 1.0, -39); // average: 2
-    unpack_half(0, QUEEN, 1.0, 640, 640, 643, 656); // average: 655
-    unpack_half(1, QUEEN, 1.0, 1274, 1329, 1268, 1333); // average: 1300
-    unpack_half(0, ROOK, 1.0, 280, 313, 286, 324); // average: 296
-    unpack_half(1, ROOK, 1.0, 629, 653, 622, 646); // average: 636
-    unpack_half(0, BISHOP, 1.0, 237, 243, 236, 250); // average: 253
-    unpack_half(1, BISHOP, 1.0, 368, 378, 369, 377); // average: 391
-    unpack_half(0, KNIGHT, 1.0, 222, 241, 225, 246); // average: 243
-    unpack_half(1, KNIGHT, 1.241, 265, 275, 265, 276); // average: 343
+    unpack_full(0, PAWN, 1.24, 10); // average: 64
+    unpack_full(1, PAWN, 1.113, 93); // average: 124
+    unpack_full(0, PASSED_PAWN, 1.069, -14); // average: 11
+    unpack_full(1, PASSED_PAWN, 1.959, -7); // average: 43
+    unpack_smol(0, KING, 1.0, -39); // average: -2
+    unpack_smol(1, KING, 1.0, -34); // average: 7
+    unpack_half(0, QUEEN, 1.0, 605, 605, 608, 621); // average: 617
+    unpack_half(1, QUEEN, 1.0, 1257, 1313, 1252, 1318); // average: 1282
+    unpack_half(0, ROOK, 1.0, 270, 303, 275, 313); // average: 285
+    unpack_half(1, ROOK, 1.0, 616, 641, 611, 634); // average: 624
+    unpack_half(0, BISHOP, 1.0, 226, 232, 225, 238); // average: 239
+    unpack_half(1, BISHOP, 1.0, 361, 370, 361, 369); // average: 380
+    unpack_half(0, KNIGHT, 1.0, 214, 232, 217, 237); // average: 233
+    unpack_half(1, KNIGHT, 1.533, 275, 286, 275, 286); // average: 369
     
     // Zobrist keys
 #ifdef OPENBENCH
