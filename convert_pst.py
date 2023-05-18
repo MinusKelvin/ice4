d2b5669036fcfb2a452826f65eb037dc419ccfbd
MinusKelvin <mark.carlson@minuskelvin.net>
2023-05-18 14:57:38 +1000

    test 768 -> 16x2 -> 1 neural network
    size: 16640
    bench: 3642181

--- convert_pst.py
+++ convert_pst.py
@@ -1,75 +1,10 @@
 #!/usr/bin/python3
 
 import json, math
-with open("0-45.json", "r") as f:
+with open("0-45-nn.json", "r") as f:
     data = json.load(f)
 
-def dump_string(piece_data, stuff, extra=None):
-    smallest = float("inf")
-    largest = -smallest
-    average = 0
-    for value in piece_data:
-        smallest = smallest if smallest < value else value
-        largest = largest if largest > value else value
-        average += value / len(piece_data)
-
-    scale_factor = max((largest - smallest) / 94, 1.0)
-
-    s = ""
-    for value in piece_data:
-        c = chr(round((value - smallest) / scale_factor) + 32)
-        if c == "\\" or c == "\"":
-            s += "\\"
-        s += c
-
-    print(f"{stuff}, {scale_factor:.4}, {round(smallest)}", end="")
-    if extra:
-        for v in extra:
-            print(f", {round(smallest + v)}", end="")
-    print(f"); // average: {average:.0f}")
-
-    return s
-
-scaled = [v * 160 for v in data["params.weight"][0]]
-
-sections = []
-sizes = [48, 16, 3, 16, 3, 16, 3, 16, 3, 16, 48, 1, 8, 1, 1, 1, 1, 1, 1, 4, 1, 1] * 2
-acc = 0
-for s in sizes:
-    sections.append(scaled[acc:acc+s])
-    acc += s
-
-eg = len(sections)//2
-
-data_string = ""
-data_string += dump_string(sections[0], "unpack_full(1, PAWN")
-data_string += dump_string(sections[eg+0], "unpack_full(0x10000, PAWN")
-data_string += dump_string(sections[10], "unpack_full(1, PASSED_PAWN")
-data_string += dump_string(sections[eg+10], "unpack_full(0x10000, PASSED_PAWN")
-data_string += dump_string(sections[9], "unpack_smol(1, KING")
-data_string += dump_string(sections[eg+9], "unpack_smol(0x10000, KING")
-data_string += dump_string(sections[7], "unpack_half(1, QUEEN", sections[8])
-data_string += dump_string(sections[eg+7], "unpack_half(0x10000, QUEEN", sections[eg+8])
-data_string += dump_string(sections[5], "unpack_half(1, ROOK", sections[6])
-data_string += dump_string(sections[eg+5], "unpack_half(0x10000, ROOK", sections[eg+6])
-data_string += dump_string(sections[3], "unpack_half(1, BISHOP", sections[4])
-data_string += dump_string(sections[eg+3], "unpack_half(0x10000, BISHOP", sections[eg+4])
-data_string += dump_string(sections[1], "unpack_half(1, KNIGHT", sections[2])
-data_string += dump_string(sections[eg+1], "unpack_half(0x10000, KNIGHT", sections[eg+2])
-
-print(f"data string: \"{data_string}\"")
-
-print(f"#define BISHOP_PAIR S({round(sections[11][0])}, {round(sections[eg+11][0])})")
-print("int32_t DOUBLED_PAWN[] = {" + ", ".join(
-    f"S({-round(v1)}, {-round(v2)})" for v1, v2 in zip(sections[12], sections[eg+12])
-) + "};")
-print(f"#define TEMPO S({round(sections[13][0])}, {round(sections[eg+13][0])})")
-print(f"#define ISOLATED_PAWN S({-round(sections[14][0])}, {-round(sections[eg+14][0])})")
-print(f"int32_t PROTECTED_PAWN[] = {{0, S({round(sections[15][0])}, {round(sections[eg+15][0])}), S({round(sections[16][0])}, {round(sections[eg+16][0])})}};")
-print(f"#define ROOK_OPEN S({round(sections[17][0])}, {round(sections[eg+17][0])})")
-print(f"#define ROOK_SEMIOPEN S({round(sections[18][0])}, {round(sections[eg+18][0])})")
-print("int32_t PAWN_SHIELD[] = {" + ", ".join(
-    f"S({round(v1)}, {round(v2)})" for v1, v2 in zip(sections[19], sections[eg+19])
-) + "};")
-print(f"#define KING_OPEN S({round(sections[20][0])}, {round(sections[eg+20][0])})")
-print(f"#define KING_SEMIOPEN S({round(sections[21][0])}, {round(sections[eg+21][0])})")
+print("FT weights:", [v for l in data["ft.weight"] for v in l])
+print("FT bias:", data["ft.bias"])
+print("out weights:", data["out.weight"][0])
+print("out bias:", data["out.bias"][0])
--- src/board.cpp
+++ src/board.cpp
@@ -42,52 +42,23 @@ vector<TtEntry> TT(HASH_SIZE);
 struct Board {
     uint8_t board[120];
     uint8_t castle_rights[2];
-    uint8_t bishops[2];
-    uint8_t king_sq[2];
-    uint8_t pawn_counts[2][10];
-    uint8_t rook_counts[2][8];
     uint8_t ep_square;
     uint8_t castle1, castle2;
     uint8_t stm;
-    uint8_t phase;
-    uint8_t pawn_eval_dirty;
-    int32_t inc_eval;
-    int32_t pawn_eval;
     uint64_t zobrist;
+    int acc[2][16];
 
     void edit(int square, int piece) {
-        if ((board[square] & 7) == PAWN || (piece & 7) == PAWN || (piece & 7) == KING) {
-            pawn_eval_dirty = 1;
+        for (int i = 0; i < 16; i++) {
+            acc[0][i] -= FT_WEIGHTS[board[square]][square-A1][i];
+            acc[1][i] -= FT_WEIGHTS[board[square] ^ INVALID][FLIP[square]-A1][i];
         }
         zobrist ^= ZOBRIST.pieces[board[square]][square-A1];
-        if ((board[square] & 7) == ROOK) {
-            rook_counts[!(board[square] & WHITE)][square % 10 - 1]--;
-        }
-        if ((board[square] & 7) == PAWN) {
-            pawn_counts[!(board[square] & WHITE)][square % 10]--;
-        } else {
-            inc_eval -= PST[board[square]][square-A1];
-        }
-        phase -= PHASE[board[square] & 7];
-        if ((board[square] & 7) == BISHOP) {
-            bishops[!(board[square] & WHITE)]--;
-        }
         board[square] = piece;
         zobrist ^= ZOBRIST.pieces[board[square]][square-A1];
-        if ((board[square] & 7) == ROOK) {
-            rook_counts[!(board[square] & WHITE)][square % 10 - 1]++;
-        }
-        if ((board[square] & 7) == PAWN) {
-            pawn_counts[!(board[square] & WHITE)][square % 10]++;
-        } else {
-            inc_eval += PST[board[square]][square-A1];
-        }
-        phase += PHASE[board[square] & 7];
-        if ((board[square] & 7) == BISHOP) {
-            bishops[!(board[square] & WHITE)]++;
-        }
-        if ((board[square] & 7) == KING) {
-            king_sq[!(board[square] & WHITE)] = square;
+        for (int i = 0; i < 16; i++) {
+            acc[0][i] += FT_WEIGHTS[board[square]][square-A1][i];
+            acc[1][i] += FT_WEIGHTS[board[square] ^ INVALID][FLIP[square]-A1][i];
         }
     }
 
@@ -107,6 +78,10 @@ struct Board {
             edit(A4 + i, EMPTY);
             edit(A5 + i, EMPTY);
             edit(A6 + i, EMPTY);
+            acc[0][2*i] += FT_BIAS[2*i];
+            acc[1][2*i] += FT_BIAS[2*i];
+            acc[0][2*i+1] += FT_BIAS[2*i+1];
+            acc[1][2*i+1] += FT_BIAS[2*i+1];
         }
     }
 
@@ -261,85 +236,16 @@ struct Board {
         return 1;
     }
 
-    void calculate_pawn_eval(int ci, int color, int pawndir, int first_rank, int seventh_rank) {
-        int shield_pawns = 0;
-        int own_pawn = PAWN | color;
-        int opp_pawn = own_pawn ^ INVALID;
-        if (!pawn_counts[ci][king_sq[ci] % 10]) {
-            pawn_eval += pawn_counts[!ci][king_sq[ci] % 10] ? KING_SEMIOPEN : KING_OPEN;
-        }
-        for (int file = 1; file < 9; file++) {
-            // Doubled pawns: 44 bytes (8117455 vs 7f7c2b5)
-            // 8.0+0.08: 5.04 +- 5.14 (2930 - 2785 - 4285) 0.11 elo/byte
-            if (pawn_counts[ci][file]) {
-                pawn_eval -= (pawn_counts[ci][file] - 1) * DOUBLED_PAWN[file - 1];
-            }
-            // Isolated pawns: 18 bytes (b4d32e5 vs 7f7c2b5)
-            // 8.0+0.08: 14.64 +- 5.20 (3128 - 2707 - 4165) 0.81 elo/byte
-            if (!pawn_counts[ci][file-1] && !pawn_counts[ci][file+1]) {
-                pawn_eval -= ISOLATED_PAWN * pawn_counts[ci][file];
-            }
-            for (int rank = seventh_rank; rank != first_rank; rank -= pawndir) {
-                int sq = file+rank;
-                if (board[sq] == own_pawn) {
-                    if (king_sq[ci] % 10 > 4) {
-                        sq = 9 + rank - file;
-                    }
-                    pawn_eval += PST[own_pawn+6][sq-A1];
-                }
-                if (board[file+rank] == opp_pawn || board[file+rank-1] == opp_pawn || board[file+rank+1] == opp_pawn) {
-                    break;
-                }
-            }
-            for (int rank = seventh_rank; rank != first_rank; rank -= pawndir) {
-                int sq = rank+file;
-                if (board[sq] == own_pawn) {
-                    int protectors = (board[sq - pawndir + 1] == own_pawn)
-                        + (board[sq - pawndir - 1] == own_pawn);
-                    pawn_eval += PROTECTED_PAWN[protectors];
-                    if (king_sq[ci] % 10 > 4) {
-                        sq = 9 + rank - file;
-                    }
-                    pawn_eval += PST[own_pawn][sq-A1];
-                }
-            }
-        }
-        // Pawn shield: 65 bytes (f3241b8 vs 7f7c2b5)
-        // 8.0+0.08: 19.58 +- 5.17 (3159 - 2596 - 4245) 0.30 elo/byte
-        for (int dx = -1; dx < 2; dx++) {
-            shield_pawns += board[king_sq[ci]+dx+pawndir] == own_pawn
-                || board[king_sq[ci]+dx+pawndir*2] == own_pawn;
-        }
-        pawn_eval += (king_sq[ci] / 10 == first_rank / 10) * PAWN_SHIELD[shield_pawns];
-    }
-
     int eval() {
-        if (pawn_eval_dirty) {
-            pawn_eval = 0;
-            calculate_pawn_eval(1, BLACK, -10, 90, 30);
-            pawn_eval = -pawn_eval;
-            calculate_pawn_eval(0, WHITE, 10, 20, 80);
-            pawn_eval_dirty = 0;
+        float v = OUT_BIAS;
+        int first = stm == BLACK;
+        for (int i = 0; i < 16; i++) {
+            v += OUT_WEIGHTS[i] * max(acc[first][i], 0);
         }
-
-        // Bishop pair: 31 bytes (ae3b5f8 vs 7f7c2b5)
-        // 8.0+0.08: 23.84 +- 5.24 (3297 - 2612 - 4091) 0.77 elo/byte
-        int bishop_pair = (bishops[0] >= 2) - (bishops[1] >= 2);
-        int e = inc_eval + pawn_eval +
-            BISHOP_PAIR * bishop_pair +
-            (stm == WHITE ? TEMPO : -TEMPO);
-        // Rook on (semi-)open file: 64 bytes (87a0681 vs 7f7c2b5)
-        // 8.0+0.08: 36.62 +- 5.35 (3594 - 2544 - 3862) 0.57 elo/byte
-        for (int file = 1; file < 9; file++) {
-            if (!pawn_counts[0][file]) {
-                e += (pawn_counts[1][file] ? ROOK_SEMIOPEN : ROOK_OPEN) * rook_counts[0][file-1];
-            }
-            if (!pawn_counts[1][file]) {
-                e -= (pawn_counts[0][file] ? ROOK_SEMIOPEN : ROOK_OPEN) * rook_counts[1][file-1];
-            }
+        for (int i = 0; i < 16; i++) {
+            v += OUT_WEIGHTS[i+16] * max(acc[!first][i], 0);
         }
-        int value = ((int16_t)e * phase + (int16_t)(e + 0x8000 >> 16) * (24 - phase)) / 24;
-        return stm == WHITE ? value : -value;
+        return v / 40;
     }
 } ROOT;
 
