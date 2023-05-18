403e5ea3418c37f37767283692233970cac509a3
MinusKelvin <mark.carlson@minuskelvin.net>
2023-05-19 09:18:44 +1000

    test 45m cpu train on 5700x with time odds
    size: 16975
    bench: 3498604

--- convert_pst.py
+++ convert_pst.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 import json, math
-with open("0-45-nn.json", "r") as f:
+with open("0-23.json", "r") as f:
     data = json.load(f)
 
-print("FT weights:", [v for l in data["ft.weight"] for v in l])
+print("FT weights:", [data["ft.weight"][i][j] for j in range(768) for i in range(16)])
 print("FT bias:", data["ft.bias"])
 print("out weights:", data["out.weight"][0])
 print("out bias:", data["out.bias"][0])
