d625823fc337260f5e5cf0824b009162d6f086ee
MinusKelvin <mark.carlson@minuskelvin.net>
2023-05-25 13:15:23 +1000

    remove pawn push to seventh rank from qsearch
    size: 4046
    bench: 4004740

--- src/board.cpp
+++ src/board.cpp
@@ -214,7 +214,7 @@ struct Board {
                 int dir = stm == WHITE ? 10 : -10;
                 int upsq = sq + dir;
                 int promo = board[upsq + dir] == INVALID ? QUEEN : 0;
-                if (!board[upsq] && (quiets || promo || board[upsq + dir + dir] == INVALID)) {
+                if (!board[upsq] && (quiets || promo)) {
                     list[count++] = Move(sq, upsq, promo);
                     if (board[sq - dir - dir] == INVALID && !board[upsq+dir]) {
                         list[count++] = Move(sq, upsq+dir, promo);
