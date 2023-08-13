3f185d28296bab8f35ab6534f391f73742756fe1
MinusKelvin <mark.carlson@minuskelvin.net>
2023-08-13 18:59:58 +1000

    train hce on The Amalgamation
    size: 4076
    bench: 3969163

--- src/tables.cpp
+++ src/tables.cpp
@@ -1,19 +1,19 @@
 #define S(a, b) (a + (b * 0x10000))
 int PST[25][SQUARE_SPAN];
-#define BISHOP_PAIR S(23, 41)
-int DOUBLED_PAWN[] = {S(8, 30), S(-8, 22), S(16, 15), S(18, 10), S(19, 8), S(14, 17), S(-4, 21), S(6, 35)};
-#define TEMPO S(6, 2)
-#define ISOLATED_PAWN S(8, 11)
-int PROTECTED_PAWN[] = {0, S(7, 7), S(9, 4)};
-#define ROOK_OPEN S(29, 11)
-#define ROOK_SEMIOPEN S(16, 16)
-int PAWN_SHIELD[] = {S(10, -25), S(19, -37), S(20, -25), S(25, -14)};
-#define KING_OPEN S(-30, -5)
-#define KING_SEMIOPEN S(-9, 19)
+#define BISHOP_PAIR S(24, 45)
+int32_t DOUBLED_PAWN[] = {S(9, 29), S(-11, 25), S(13, 21), S(16, 14), S(19, 10), S(13, 21), S(-8, 24), S(8, 36)};
+#define TEMPO S(8, 4)
+#define ISOLATED_PAWN S(9, 9)
+int32_t PROTECTED_PAWN[] = {0, S(8, 6), S(9, 3)};
+#define ROOK_OPEN S(35, 9)
+#define ROOK_SEMIOPEN S(18, 15)
+int32_t PAWN_SHIELD[] = {S(13, -22), S(22, -39), S(24, -29), S(30, -22)};
+#define KING_OPEN S(-39, -2)
+#define KING_SEMIOPEN S(-11, 20)
 
 int PHASE[] = {0, 0, 1, 1, 2, 4, 0};
 
-const char *DATA_STRING = "EY\\@<A93HVNFCD98HO[XTM=;OXgc`VKE[c{vqgVK@ i~vl7h%(0642,.! ,,,('%%)'&%(,(-/.+-0216C9??=>:aHn~yurz.932.+*0,-%&  +/.-#!$&46-6/025C=3*63<DKQSNAGW\\~V#$  ##+\"'-'%$&*'694/+,13EF>6038?XUO;6<GPe~Z5/5DBO>@_6 \"4KPG7wv[C&2. 5LI2VbcTVhg[ '),.26245423-209.% -/,4,CNK2MWc139? )21*512('01 &&\"()&($%(''.,)#  !-73..<8611:D 0&-+3872=EG,AGJ #).)-94+?=E;8HG BUXL\\biVirx_qz~";
+const char *DATA_STRING = "9MQ3.5.$>KC;89-+?DPNKB3/GO^ZUI@:W\\vkeZJC; g{~j8h%*2>9602! .00)(&$)'%%),),..),2225E<ED@B<[^qsok~h3>7820083.'$%#-744$ '(:@3:569>LF5.:;CKVYZXCJJT~R''# \"$-\"*4,(#(.):A;50057MOE;57>Dd^V?;EOXu~bD?GFWVAB`?! 4aQGCm~lU&3. 1IH0@SVB+:@0 ')-/3;557643/20.(% '()5(@NL3NYg25<C /96+:57)+24 %'\")'%'#\"&&&,+)\"% #2<910@=;34@J )#,!-2508BE)?DH \"-1+0@9.EDN?=PO FV[P^akXirwar{~";
 
 void unpack_full(int phase, int piece, double scale, int offset) {
     for (int rank = 10; rank < 70; rank+=10) {
@@ -83,20 +83,20 @@ struct Zobrist {
 } ZOBRIST;
 
 void init_tables() {
-    unpack_full(1, PAWN, 1.088, 10); // average: 64
-    unpack_full(0x10000, PAWN, 1.337, 93); // average: 126
-    unpack_full(1, PASSED_PAWN, 1.251, -14); // average: 14
-    unpack_full(0x10000, PASSED_PAWN, 1.979, -5); // average: 40
-    unpack_smol(1, KING, 1.0, -38); // average: 2
-    unpack_smol(0x10000, KING, 1.0, -39); // average: 2
-    unpack_half(1, QUEEN, 1.0, 640, 640, 643, 656); // average: 655
-    unpack_half(0x10000, QUEEN, 1.0, 1274, 1329, 1268, 1333); // average: 1300
-    unpack_half(1, ROOK, 1.0, 280, 313, 286, 324); // average: 296
-    unpack_half(0x10000, ROOK, 1.0, 629, 653, 622, 646); // average: 636
-    unpack_half(1, BISHOP, 1.0, 237, 243, 236, 250); // average: 253
-    unpack_half(0x10000, BISHOP, 1.0, 368, 378, 369, 377); // average: 391
-    unpack_half(1, KNIGHT, 1.0, 222, 241, 225, 246); // average: 243
-    unpack_half(0x10000, KNIGHT, 1.241, 265, 275, 265, 276); // average: 343
+    unpack_full(1, PAWN, 1.193, 24); // average: 72
+    unpack_full(0x10000, PAWN, 1.164, 85); // average: 116
+    unpack_full(1, PASSED_PAWN, 1.061, -18); // average: 11
+    unpack_full(0x10000, PASSED_PAWN, 1.572, -6); // average: 40
+    unpack_smol(1, KING, 1.149, -48); // average: 4
+    unpack_smol(0x10000, KING, 1.0, -24); // average: 2
+    unpack_half(1, QUEEN, 1.0, 604, 604, 608, 628); // average: 621
+    unpack_half(0x10000, QUEEN, 1.0, 1142, 1187, 1139, 1193); // average: 1166
+    unpack_half(1, ROOK, 1.0, 312, 344, 319, 363); // average: 331
+    unpack_half(0x10000, ROOK, 1.0, 549, 568, 541, 557); // average: 555
+    unpack_half(1, BISHOP, 1.0, 261, 269, 261, 277); // average: 281
+    unpack_half(0x10000, BISHOP, 1.0, 306, 312, 307, 311); // average: 325
+    unpack_half(1, KNIGHT, 1.0, 240, 262, 245, 270); // average: 265
+    unpack_half(0x10000, KNIGHT, 1.363, 200, 204, 199, 204); // average: 288
     
     // Zobrist keys
 #ifdef OPENBENCH
