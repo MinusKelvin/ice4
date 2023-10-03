7db34abc04302bff6be5795d6c28eaac7e5b07ae
MinusKelvin <mark.carlson@minuskelvin.net>
2023-10-03 11:50:16 +1100

    increase history change when eval is <= alpha
    size: 4086
    bench: 4440042

--- src/search.cpp
+++ src/search.cpp
@@ -211,7 +211,8 @@ struct Searcher {
                 }
                 if (v >= beta) {
                     if (!victim) {
-                        int change = depth * depth;
+                        int d = depth + (eval <= alpha);
+                        int change = d * d;
                         int16_t *hist;
                         for (int j = 0; j < i; j++) {
                             if (board.board[moves[j].to]) {
