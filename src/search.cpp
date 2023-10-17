3d46ce33ac9b89c0ab0b5217fd8434e69cdbbe9a
MinusKelvin <mark.carlson@minuskelvin.net>
2023-10-17 22:16:36 +1100

    butterfly history
    size: 4120
    bench: 2239179

--- src/search.cpp
+++ src/search.cpp
@@ -17,7 +17,7 @@ struct Searcher {
     uint64_t nodes;
     double abort_time;
     int16_t evals[256];
-    HTable history;
+    int16_t history[2][SQUARE_SPAN][SQUARE_SPAN];
     HTable conthist[14][SQUARE_SPAN];
     HTable *conthist_stack[256];
     uint64_t rep_list[256];
@@ -201,7 +201,7 @@ struct Searcher {
                     v = -negamax(mkmove, scratch, -beta, -alpha, depth - 1 + in_check, ply + 1);
                 }
                 if (v == LOST) {
-                    moves[i].from = 1;
+                    moves[i].from = moves[i].to;
                 } else {
                     legals++;
                 }
@@ -221,7 +221,7 @@ struct Searcher {
                             if (board.board[moves[j].to]) {
                                 continue;
                             }
-                            hist = &history[board.board[moves[j].from] - WHITE_PAWN][moves[j].to-A1];
+                            hist = &history[board.stm == WHITE][moves[j].from-A1][moves[j].to-A1];
                             *hist -= change + change * *hist / MAX_HIST;
                             if (ply) {
                                 hist = &(*conthist_stack[ply - 1])[board.board[moves[j].from] - WHITE_PAWN][moves[j].to-A1];
@@ -232,7 +232,7 @@ struct Searcher {
                                 *hist -= change + change * *hist / MAX_HIST;
                             }
                         }
-                        hist = &history[board.board[moves[i].from] - WHITE_PAWN][moves[i].to-A1];
+                        hist = &history[board.stm == WHITE][moves[i].from - A1][moves[i].to-A1];
                         *hist += change - change * *hist / MAX_HIST;
                         if (ply) {
                             hist = &(*conthist_stack[ply - 1])[board.board[moves[i].from] - WHITE_PAWN][moves[i].to-A1];
@@ -270,7 +270,7 @@ struct Searcher {
                         // Plain history: 28 bytes (676e7fa vs 4cabdf1)
                         // 8.0+0.08: 51.98 +- 5.13 (3566 - 2081 - 4353) 1.86 elo/byte
                         // 60.0+0.6: 52.37 +- 4.62 (3057 - 1561 - 5382) 1.87 elo/byte
-                        score[j] = history[board.board[moves[j].from] - WHITE_PAWN][moves[j].to-A1]
+                        score[j] = history[board.stm == WHITE][moves[j].from - A1][moves[j].to-A1]
                             // Continuation histories: 87 bytes (af63703 vs 4cabdf1)
                             // 8.0+0.08: 22.93 +- 5.09 (3124 - 2465 - 4411) 0.26 elo/byte
                             // 60.0+0.6: 46.52 +- 4.57 (2930 - 1599 - 5471) 0.53 elo/byte
