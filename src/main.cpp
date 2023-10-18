b110249c639553538c99326cf78335e06a85ecec
MinusKelvin <mark.carlson@minuskelvin.net>
2023-10-18 13:36:51 +1100

    increase bench depth
    size: 4096
    bench: 6163441

--- src/main.cpp
+++ src/main.cpp
@@ -104,7 +104,7 @@ int main(int argc, char *argv[]) {
             ROOT = Board();
             parse_fen();
             Searcher s;
-            s.iterative_deepening(1.0/0.0, 10);
+            s.iterative_deepening(1.0/0.0, 12);
             nodes += s.nodes;
         }
         clock_gettime(CLOCK_MONOTONIC, &end);
