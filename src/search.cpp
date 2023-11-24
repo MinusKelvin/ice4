2b498cb18902281fdfd1019eace4a78638ffdcaa
MinusKelvin <mark.carlson@minuskelvin.net>
2023-11-24 23:26:52 +1100

    cap reduction to prevent reducing into qsearch
    size: 4073
    bench: 5631749

--- src/search.cpp
+++ src/search.cpp
@@ -225,6 +225,9 @@ struct Searcher {
                 // 8.0+0.08: 17.60 +- 5.06 (3011 - 2505 - 4484) 2.93 elo/byte
                 // 60.0+0.6: 48.01 +- 4.69 (3062 - 1689 - 5249) 8.00 elo/byte
                 reduction -= score[i] / 580;
+                if (reduction > depth - 2) {
+                    reduction = depth - 2;
+                }
                 if (reduction < 0 || victim || in_check) {
                     reduction = 0;
                 }
