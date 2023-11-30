5ed0a019cb48ee121142d1678f24679695840438
MinusKelvin <mark.carlson@minuskelvin.net>
2023-12-01 10:45:47 +1100

    do not apply pawn protected pruning until a legal move
    size: 4068
    bench: 5638382

--- src/search.cpp
+++ src/search.cpp
@@ -172,7 +172,7 @@ struct Searcher {
             int pawn_attacked =
                 board.board[moves[i].to + (board.stm & WHITE ? 11 : -11)] == opp_pawn ||
                 board.board[moves[i].to + (board.stm & WHITE ? 9 : -9)] == opp_pawn;
-            if (pawn_attacked && (board.board[moves[i].from] & 7) > victim + max(0, depth) / 2) {
+            if (legals && pawn_attacked && (board.board[moves[i].from] & 7) > victim + max(0, depth) / 2) {
                 continue;
             }
 
