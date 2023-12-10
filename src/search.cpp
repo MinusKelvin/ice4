ecb32f2ee30eceb23bf54825b01abd61a3e03c93
MinusKelvin <mark.carlson@minuskelvin.net>
2023-12-11 00:24:40 +1100

    tweak lmr manually
    size: 4065
    bench: 5548960

--- src/search.cpp
+++ src/search.cpp
@@ -203,11 +203,11 @@ struct Searcher {
                 // All reductions: 41 bytes (cedac94 vs b915a59)
                 // 8.0+0.08: 184.70 +- 6.16 (5965 - 1099 - 2936) 4.50 elo/byte
                 // 60.0+0.6: 213.11 +- 6.04 (6132 - 667 - 3201) 5.20 elo/byte
-                int reduction = legals * 0.114 + depth * 0.152;
+                int reduction = legals * 0.22 + depth * 0.12 - 1;
                 // Extra reduction condition: 5 bytes (e61a8aa vs 0e2f650)
                 // 8.0+0.08: 22.65 +- 5.17 (3207 - 2556 - 4237) 4.53 elo/byte
                 // 60.0+0.6: 14.32 +- 4.67 (2557 - 2145 - 5298) 2.86 elo/byte
-                reduction += legals > 7;
+                reduction += legals > 5;
                 // History Reduction: 6 bytes (bf488d7 vs 0e2f650)
                 // 8.0+0.08: 17.60 +- 5.06 (3011 - 2505 - 4484) 2.93 elo/byte
                 // 60.0+0.6: 48.01 +- 4.69 (3062 - 1689 - 5249) 8.00 elo/byte
