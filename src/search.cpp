416accf68fb6a7e383a05dc63d785db2e1e525c6
MinusKelvin <mark.carlson@minuskelvin.net>
2023-12-24 09:31:55 +1100

    depth-dependant aspiration window size
    size: 4076
    bench: 4733628

--- src/search.cpp
+++ src/search.cpp
@@ -300,7 +300,7 @@ struct Searcher {
         int last_score = 0;
         try {
             for (int depth = 1; depth <= max_depth; depth++) {
-                int window = 7;
+                int window = 4 + 25 / depth;
                 int v = last_score + window;
                 while (v <= last_score - window || v >= last_score + window) {
                     window *= 2;
