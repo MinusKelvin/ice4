777bf483171458941fcf55491a2027ef5e0095e6
MinusKelvin <mark.carlson@minuskelvin.net>
2024-02-09 11:21:47 +1100

    more iir at pv nodes
    size: 4024
    bench: 4356088

--- src/search.cpp
+++ src/search.cpp
@@ -51,7 +51,7 @@ struct Searcher {
             // Internal Iterative Reductions: 8 bytes (524f0e8 vs b5fdb00)
             // 8.0+0.08: 0.66 +- 5.07 (2790 - 2771 - 4439) 0.08 elo/byte
             // 60.0+0.6: 22.30 +- 4.52 (2530 - 1889 - 5581) 2.79 elo/byte
-            depth--;
+            depth -= 1 + pv;
         }
 
         board.movegen(moves, mvcount, depth > 0, mobilities[ply+1]);
