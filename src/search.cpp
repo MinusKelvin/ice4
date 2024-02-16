dda4bae32dd7fb277666c73ab6ed08c6750f8189
MinusKelvin <mark.carlson@minuskelvin.net>
2024-02-16 13:52:24 +1100

    test extra reduction condition for elo
    size: 4056
    bench: 5760228

--- src/search.cpp
+++ src/search.cpp
@@ -204,10 +204,6 @@ struct Searcher {
                 // 8.0+0.08: 184.70 +- 6.16 (5965 - 1099 - 2936) 4.50 elo/byte
                 // 60.0+0.6: 213.11 +- 6.04 (6132 - 667 - 3201) 5.20 elo/byte
                 int reduction = legals * 0.114 + depth * 0.152;
-                // Extra reduction condition: 5 bytes (e61a8aa vs 0e2f650)
-                // 8.0+0.08: 22.65 +- 5.17 (3207 - 2556 - 4237) 4.53 elo/byte
-                // 60.0+0.6: 14.32 +- 4.67 (2557 - 2145 - 5298) 2.86 elo/byte
-                reduction += legals > 7;
                 // History Reduction: 6 bytes (bf488d7 vs 0e2f650)
                 // 8.0+0.08: 17.60 +- 5.06 (3011 - 2505 - 4484) 2.93 elo/byte
                 // 60.0+0.6: 48.01 +- 4.69 (3062 - 1689 - 5249) 8.00 elo/byte
