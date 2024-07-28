ef2c70e555b601314c33aec0cc0456fc0c5c3775
MinusKelvin <mark.carlson@minuskelvin.net>
2024-07-28 10:09:54 +1000

    reduce more if ttmove is noisy
    size: 4064
    bench: 3644016

--- src/search.cpp
+++ src/search.cpp
@@ -208,6 +208,7 @@ struct Searcher {
                 // 8.0+0.08: 80.97 +- 5.10     8.10 elo/byte
                 // 60.0+0.6: 83.09 +- 4.65     8.31 elo/byte
                 int reduction = legals * 0.155 + depth * 0.165;
+                reduction += hashmv.from && board.board[hashmv.to];
                 // History reduction: 9 bytes (v4)
                 // 8.0+0.08: 26.28 +- 2.98     2.92 elo/byte
                 // 60.0+0.6: 37.09 +- 2.65     4.12 elo/byte
