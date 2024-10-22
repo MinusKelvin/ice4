a13b5997f3351be2046e3d6185d6ee543d9981ff
MinusKelvin <mark.carlson@minuskelvin.net>
2024-10-22 17:10:56 +1100

    nmp reduction based on eval-beta margin
    size: 3602
    bench: 3231370

--- src/search.cpp
+++ src/search.cpp
@@ -66,7 +66,11 @@ struct Searcher {
 
             conthist_stack[ply + 2] = &conthist[0][0];
 
-            int v = -negamax(mkmove, scratch, -beta, -alpha, 0.75 * depth - 3, ply + 1);
+            int v = -negamax(
+                mkmove, scratch, -beta, -alpha,
+                (75 * depth - min(eval - beta, 300) - 300) / 100,
+                ply + 1
+            );
             if (v >= beta) {
                 return v;
             }
