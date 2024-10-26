10d4bed35334dca392ab4d46b26e5df80ec0c6aa
MinusKelvin <mark.carlson@minuskelvin.net>
2024-10-27 08:33:01 +1100

    increase bench depth to 15
    size: 4023
    bench: 10352388

--- src/main.cpp
+++ src/main.cpp
@@ -112,7 +112,7 @@ int main(int argc, char *argv[]) {
             ROOT = Board();
             parse_fen();
             Searcher s{};
-            s.iterative_deepening(INT_MAX, 12);
+            s.iterative_deepening(INT_MAX, 15);
             nodes += s.nodes;
         }
         clock_gettime(CLOCK_MONOTONIC, &end);
