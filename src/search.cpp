887818d888052caa4d92ec944a70d66fbf41480b
MinusKelvin <mark.carlson@minuskelvin.net>
2024-11-01 23:41:03 +1100

    order losing captures after other captures
    size: 4258
    bench: 8085846

--- src/search.cpp
+++ src/search.cpp
@@ -124,7 +124,9 @@ struct Searcher {
                 // MVV-LVA capture ordering: 3 bytes (78a3963 vs 35f9b66)
                 // 8.0+0.08: 289.03 +- 7.40 (7378 - 563 - 2059) 96.34 elo/byte
                 // 60.0+0.6: 237.53 +- 6.10 (6384 - 445 - 3171) 79.18 elo/byte
-                score[j] = board.board[moves[j].to] * 1e5 - board.board[moves[j].from];
+                score[j] = board.board[moves[j].to] * 1e5
+                    - board.board[moves[j].from]
+                    + 25e5 * board.see_ge(moves[j], 0);
             } else {
                 // Plain history: 28 bytes (676e7fa vs 4cabdf1)
                 // 8.0+0.08: 51.98 +- 5.13 (3566 - 2081 - 4353) 1.86 elo/byte
