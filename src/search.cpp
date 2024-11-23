1816c80a1d6c0cf5d827ce4b98c543c8a8256817
MinusKelvin <mark.carlson@minuskelvin.net>
2024-11-23 19:37:14 +1100

    remove improving from lmp take 2
    size: 4041
    bench: 9046330

--- src/search.cpp
+++ src/search.cpp
@@ -149,7 +149,7 @@ struct Searcher {
         }
 
         int best = depth ? LOST + ply : eval;
-        int quiets_to_check = pv ? -1 : (depth*depth + 10) >> (!improving + 1);
+        int quiets_to_check = pv ? -1 : 0.25 * depth*depth + 2.5;
         int raised_alpha = 0;
         int legals = 0;
 
