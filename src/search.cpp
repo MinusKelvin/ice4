d1706661afeb15ca2260c2f51e128809adba91e4
MinusKelvin <mark.carlson@minuskelvin.net>
2024-12-08 22:22:35 +1100

    do iir after pruning
    size: 4041
    bench: 9756238

--- src/search.cpp
+++ src/search.cpp
@@ -57,11 +57,6 @@ struct Searcher {
                 bestmv = tt.mv;
                 return tt.eval;
             }
-        } else if (depth > 3) {
-            // Internal Iterative Reductions: 6 bytes (v4)
-            // 8.0+0.08: 36.52 +- 3.00    6.09 elo/byte
-            // 60.0+0.6: 40.34 +- 2.64    6.72 elo/byte
-            depth -= 2;
         }
 
         board.movegen(moves, mvcount, depth, mobilities[ply+1]);
@@ -111,6 +106,13 @@ struct Searcher {
             if (v <= alpha) {
                 return v;
             }
+        }
+
+        if (!tt_good && depth > 3) {
+            // Internal Iterative Reductions: 6 bytes (v4)
+            // 8.0+0.08: 36.52 +- 3.00    6.09 elo/byte
+            // 60.0+0.6: 40.34 +- 2.64    6.72 elo/byte
+            depth -= 2;
         }
 
         // Internal Iterative Deepening: 16 bytes (v4)
