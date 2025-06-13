9a3c4c26b7908352468d1f0a1ab27680841695df
MinusKelvin <mark.carlson@minuskelvin.net>
2025-06-14 08:43:15 +1000

    disable IIR during singular search
    size: 4163
    bench: 9818101

--- src/search.cpp
+++ src/search.cpp
@@ -55,7 +55,7 @@ struct Searcher {
                 bestmv = tt.mv;
                 return tt.eval;
             }
-        } else if (depth > 3) {
+        } else if (!excluded.from && depth > 3) {
             // Internal Iterative Reductions: 6 bytes (v4)
             // 8.0+0.08: 36.52 +- 3.00    6.09 elo/byte
             // 60.0+0.6: 40.34 +- 2.64    6.72 elo/byte
