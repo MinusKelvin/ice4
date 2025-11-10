10509ada3cfb3eaede5b0503f6d2ca6a9cfd50cf
MinusKelvin <mark.carlson@minuskelvin.net>
2025-11-10 15:35:54 +1100

    don't do lmp in pv
    size: 3587
    bench: 2648062

--- src/search.cpp
+++ src/search.cpp
@@ -106,7 +106,7 @@ struct Searcher {
                 continue;
             }
 
-            if (!victim && !(quiets_to_check -= 1)) {
+            if (!pv && !victim && !(quiets_to_check -= 1)) {
                 break;
             }
 
