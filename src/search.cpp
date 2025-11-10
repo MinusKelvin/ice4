80ce352ad6d5aac460a99e40383a32d6c01f4867
MinusKelvin <mark.carlson@minuskelvin.net>
2025-11-11 10:43:18 +1100

    reduce less in pv nodes plus param tweak
    size: 3668
    bench: 6586109

--- src/search.cpp
+++ src/search.cpp
@@ -135,9 +135,9 @@ struct Searcher {
             if (is_rep) {
                 v = 0;
             } else if (legals) {
-                int reduction = LOG[legals] * LOG[depth] * 0.69 - 0.11;
+                int reduction = LOG[legals] * LOG[depth] * 0.7 + 0.2 - pv;
 
-                if (victim) {
+                if (victim || reduction < 0) {
                     reduction = 0;
                 }
 
