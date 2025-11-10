fa0c79e59fdf076a936d6150ad342e7b1a072a17
MinusKelvin <mark.carlson@minuskelvin.net>
2025-11-11 09:07:15 +1100

    lmr param tweak
    size: 3657
    bench: 7455111

--- src/search.cpp
+++ src/search.cpp
@@ -135,7 +135,7 @@ struct Searcher {
             if (is_rep) {
                 v = 0;
             } else if (legals) {
-                int reduction = LOG[legals] * LOG[depth] * 0.69 - 0.11;
+                int reduction = LOG[legals] * LOG[depth] * 0.4 + 0.8;
 
                 if (victim) {
                     reduction = 0;
