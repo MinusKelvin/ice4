73a504f1c81a11b2b269df0d679bd585bea0ecc2
MinusKelvin <mark.carlson@minuskelvin.net>
2025-11-29 10:27:13 +1100

    reduce moves that give check less
    size: 3798
    bench: 6763922

--- src/search.cpp
+++ src/search.cpp
@@ -142,9 +142,11 @@ struct Searcher {
             if (is_rep) {
                 v = 0;
             } else if (legals) {
-                int reduction = LOG[legals] * LOG[depth] * 0.69 - 0.11;
+                int reduction = -0.11
+                    + 0.69 * LOG[legals] * LOG[depth]
+                    - mkmove.check;
 
-                if (victim) {
+                if (victim || reduction < 0) {
                     reduction = 0;
                 }
 
