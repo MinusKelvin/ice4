fe8bfb53c80276dd8275e7376695924bba2b6c17
MinusKelvin <mark.carlson@minuskelvin.net>
2025-12-02 10:13:18 +1100

    continuation corrhist 2 ply
    size: 3900
    bench: 7177433

--- src/search.cpp
+++ src/search.cpp
@@ -57,7 +57,8 @@ struct Searcher {
         int eval = board.eval(mobilities[ply+1] - mobilities[ply] + TEMPO)
             + corr_hist[ply & 1][board.pawn_hash % CORR_HIST_SIZE] / 256
             + corr_hist[ply & 1][board.material_hash % CORR_HIST_SIZE] / 256
-            + (*conthist_stack[ply+1])[0][0] / 256;
+            + (*conthist_stack[ply+1])[0][0] / 256
+            + (*conthist_stack[ply])[0][1] / 256;
         int improving = ply > 1 && !board.check && eval > evals[ply-2];
         evals[ply] = board.check ? WON : eval;
         rep_list[ply] = board.zobrist;
@@ -243,6 +244,8 @@ struct Searcher {
                 hist = &corr_hist[ply & 1][board.material_hash % CORR_HIST_SIZE];
                 *hist += bonus - abs(bonus) * *hist / MAX_HIST;
                 hist = &(*conthist_stack[ply+1])[0][0];
+                *hist += bonus - abs(bonus) * *hist / MAX_HIST;
+                hist = &(*conthist_stack[ply])[0][1];
                 *hist += bonus - abs(bonus) * *hist / MAX_HIST;
             }
         }
